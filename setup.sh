b85e1dbc03a6c8ebd19b3431561bf3ee4f5e4ecf
Mohamed Elsayed <mohamed20020093@gmail.com>
2026-02-04 15:10:34 +0200

    Update branch versions in setup.sh to lineage-23.2

--- setup.sh
+++ setup.sh
@@ -13,8 +13,8 @@ rm -rf hardware/oplus
 rm -rf hardware/pixelworks/interfaces
 
 #device-tree
-git clone https://github.com/asmodeus-oos/android_device_realme_ferrari.git -b lineage-23.0 device/realme/ferrari
-git clone https://github.com/asmodeus-oos/android_device_oneplus_sm8450-common.git -b lineage-23.0 device/oneplus/sm8450-common
+git clone https://github.com/asmodeus-oos/android_device_realme_ferrari.git -b lineage-23.2 device/realme/ferrari
+git clone https://github.com/asmodeus-oos/android_device_oneplus_sm8450-common.git -b lineage-23.2 device/oneplus/sm8450-common
 
 #vendor
 git clone https://gitlab.com/asmodeus-oos/proprietary_vendor_realme_ferrari.git -b lineage-23.0 vendor/realme/ferrari
@@ -26,5 +26,5 @@ git clone https://github.com/asmodeus-oos/android_kernel_oneplus_sm8450-modules.
 git clone https://github.com/asmodeus-oos/android_kernel_oneplus_sm8450-devicetrees.git -b lineage-23.0 kernel/oneplus/sm8450-devicetrees
 
 #hardware
-git clone https://github.com/asmodeus-oos/android_hardware_oplus.git -b lineage-23.0 hardware/oplus
+git clone https://github.com/asmodeus-oos/android_hardware_oplus.git -b lineage-23.2 hardware/oplus
 git clone https://github.com/asmodeus-oos/hardware_pixelworks_interfaces.git -b lineage-22.2 hardware/pixelworks/interfaces
