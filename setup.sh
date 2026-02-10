52be6222c413d5d72c18a725fdda986cb17df31a
Mohamed Elsayed <mohamed20020093@gmail.com>
2026-02-10 13:53:06 +0200

    Update vendor repos to lineage-23.2 and add patches
    Updated vendor repositories to use lineage-23.2 branch and added patches for audio and display.

--- setup.sh
+++ setup.sh
@@ -16,8 +16,8 @@ git clone https://github.com/asmodeus-oos/android_device_realme_ferrari.git -b l
 git clone https://github.com/asmodeus-oos/android_device_oneplus_sm8450-common.git -b lineage-23.2 device/oneplus/sm8450-common
 
 #vendor
-git clone https://gitlab.com/asmodeus-oos/proprietary_vendor_realme_ferrari.git -b lineage-23.0 vendor/realme/ferrari
-git clone https://gitlab.com/asmodeus-oos/proprietary_vendor_oneplus_sm8450-common.git -b lineage-23.0 vendor/oneplus/sm8450-common
+git clone https://gitlab.com/asmodeus-oos/proprietary_vendor_realme_ferrari.git -b lineage-23.2 vendor/realme/ferrari
+git clone https://gitlab.com/asmodeus-oos/proprietary_vendor_oneplus_sm8450-common.git -b lineage-23.2 vendor/oneplus/sm8450-common
 
 #kernel
 git clone https://github.com/asmodeus-oos/android_kernel_oneplus_sm8450.git -b lineage-23.2 kernel/oneplus/sm8450
@@ -26,3 +26,12 @@ git clone https://github.com/asmodeus-oos/android_kernel_oneplus_sm8450-devicetr
 
 #hardware
 git clone https://github.com/asmodeus-oos/android_hardware_oplus.git -b lineage-23.2 hardware/oplus
+
+#Patches
+cd hardware/qcom-caf/sm8450/audio/agm
+git fetch https://github.com/LineageOS/android_vendor_qcom_opensource_agm refs/changes/20/417220/3 && git cherry-pick FETCH_HEAD
+cd ../../display
+git fetch https://github.com/LineageOS/android_hardware_qcom_display refs/changes/30/432430/9 && git cherry-pick FETCH_HEAD
+git fetch https://github.com/LineageOS/android_hardware_qcom_display refs/changes/31/432431/12 && git cherry-pick FETCH_HEAD
+git fetch https://github.com/LineageOS/android_hardware_qcom_display refs/changes/32/432432/10 && git cherry-pick FETCH_HEAD
+cd ../../../../
